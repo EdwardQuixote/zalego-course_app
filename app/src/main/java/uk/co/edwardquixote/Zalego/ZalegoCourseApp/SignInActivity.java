710710b8c4bac07d6ee9330193c0708136d2493c
Edward Ndukui <edwardquixote>
2020-05-08 11:51:52 +0300

    Implemented communication to API for User Sign In using Android Volley library.

--- app/src/main/java/uk/co/edwardquixote/Zalego/ZalegoCourseApp/SignInActivity.java
+++ app/src/main/java/uk/co/edwardquixote/Zalego/ZalegoCourseApp/SignInActivity.java
@@ -2,14 +2,26 @@ package uk.co.edwardquixote.Zalego.ZalegoCourseApp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -62,6 +74,73 @@ public class SignInActivity extends AppCompatActivity {
 
     }
 
+    private boolean codeToValidateUserInput() {
+
+        if (TextUtils.isEmpty(tiedEmailAddress.getText())) {
+
+            tilEmailAddress.setError("Provide Email Address!");
+
+            return false;
+        } else if (TextUtils.isEmpty(tiedPassword.getText())) {
+
+            tilPassword.setError("Provide password!");
+
+            return false;
+        } else {
+
+            tilEmailAddress.setError(null);
+            tilPassword.setError(null);
+
+            return true;
+        }
+    }
+
+    private void signInUser() {
+
+        final String userEmailAddress = tiedEmailAddress.getText().toString();
+        final String userPassword = tiedPassword.getText().toString();
+
+        String signInURL = "http://localhost:80/zalegocourseapp/login.php";
+
+        StringRequest request = new StringRequest(Request.Method.POST, signInURL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+
+                Log.e("SignInActivity", "Sign In response: " + response);
+
+                codeToStartCourseListActivity();
+
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.e("SignInActivity", "Sign In error: " + error);
+
+            }
+
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                super.getParams();
+
+                Map<String, String> mapRequestParameters = new HashMap<>();
+                mapRequestParameters.put("user_email_address", userEmailAddress);
+                mapRequestParameters.put("password", userPassword);
+
+                return mapRequestParameters;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(SignInActivity.this);
+        requestQueue.add(request);
+
+    }
+
 
     private View.OnClickListener clkSignIn = new View.OnClickListener() {
 
@@ -78,9 +157,11 @@ public class SignInActivity extends AppCompatActivity {
 
                 case R.id.btnSignIn:
 
-                    //  TODO: Sign In user here.
+                    if (codeToValidateUserInput()) {
 
-                    codeToStartCourseListActivity();
+                        signInUser();
+
+                    }
 
                     break;
 
